0159b5fb074fabaf06a3e7e58c9ee3d26607b302
webghzyt <engrohitjain5@gmail.com>
2015-05-28 13:03:07 +0530

    more implemention-3

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -29,11 +29,19 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
+  config.assets.compress = true
+
+  # Don't fallback to assets pipeline if a precompiled asset is missed
   config.assets.compile = true
+
+  # Generate digests for assets URLs
+  config.assets.digest = true
+  config.force_ssl = true
+  
+
 
   # Asset digests allow you to set far-future HTTP expiration dates on all assets,
   # yet still be able to expire them through the digest params.
-  config.assets.digest = true
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
